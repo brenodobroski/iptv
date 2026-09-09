fe736bf4e3e2d2d1d4e0b18ef7b7a639a65140bd
PEN-001367 <pen-001367@jcm.local>
2026-09-09 16:12:49 -0300

    ajust sw js

--- sw.js
+++ sw.js
@@ -1,8 +1,13 @@
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Instalado com sucesso.');
+    console.log('[Service Worker] Instalado com sucesso.'); //[cite: 8]
 });
 
 self.addEventListener('fetch', (e) => {
-    // Permite que as requisições fluam normalmente sem cache forçado
+    // Se a requisição for para um domínio externo (como o TMDB), ignora o Service Worker
+    if (!e.request.url.startsWith(self.location.origin)) {
+        return; 
+    }
+
+    // Permite que as requisições locais fluam normalmente[cite: 8]
     e.respondWith(fetch(e.request));
 });
